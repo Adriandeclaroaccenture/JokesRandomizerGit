4f95246762509d98735182b8a659897c0b17d4b1
Adriandeclaroaccenture <john.adrian.de.claro@accenture.com>
2022-11-29 20:03:42 +0800

    <Add: Coredata calling; Fix: API Call >

---
+++ JokeRandomizer/JokeRandomizer/Database/CoreDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataManager.swift
+//  JokeRandomizer
+//
+//  Created by john.adrian.de.claro on 11/29/22.
+//
+
+import Foundation
---
+++ JokeRandomizer/JokeRandomizer/Generic Classess/DataProvider.swift
@@ -0,0 +1,8 @@
+//
+//  DataProvider.swift
+//  JokeRandomizer
+//
+//  Created by john.adrian.de.claro on 11/29/22.
+//
+
+import Foundation
